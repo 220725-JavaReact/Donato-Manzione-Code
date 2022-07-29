d03bb765e5585dd39a816bc5e0c4786b4e71a588
Donato <donato@Users-iMac.attlocal.net>
2022-07-28 22:42:42 -0400

    most of proproperties of model classes added - preliminary ideas, subject to change

--- src/com/revature/pirateRev/models/LineItem.java
+++ src/com/revature/pirateRev/models/LineItem.java
@@ -1,5 +1,6 @@
 package com.revature.pirateRev.models;
 
 public class LineItem {
-
+	private Product product;
+	private int quantity;
 }
--- src/com/revature/pirateRev/models/Product.java
+++ src/com/revature/pirateRev/models/Product.java
@@ -1,5 +1,10 @@
 package com.revature.pirateRev.models;
 
 public class Product {
-
+	private String name;
+	private double price;
+	private String description;
+	private enum category {
+		trinkets, clothingAndAccessories, magicTokensAndPotions, navigation, weaponry, howToGuides, miscellaneous
+	}
 }
