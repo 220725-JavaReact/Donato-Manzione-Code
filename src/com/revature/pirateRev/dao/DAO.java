57ec2d2d4ef7077c4277a8be93e8d021c8dba631
Donato <donato.manz.sc@gmail.com>
2022-08-01 16:44:58 -0400

    DAO interface, dao package, and PirateDAO class without implementations

---
+++ src/com/revature/pirateRev/dao/DAO.java
@@ -0,0 +1,8 @@
+package com.revature.pirateRev.dao;
+
+public interface DAO<T> {
+	public void create(T obj);
+	public T readByName(String name);
+	public void update(T obj);
+	public void delete(String name);
+}
--- src/com/revature/pirateRev/ui/Menu.java
+++ src/com/revature/pirateRev/ui/Menu.java
@@ -6,5 +6,11 @@ public class Menu {
 
 	public static void open() {
 		Scanner sc = new Scanner(System.in);
+		String pirateInput = "";
+		do {
+			System.out.println("Welcome to Pirate Supply Store!");
+			
+			
+		}while(!pirateInput.equals("x"));
 	}
 }
