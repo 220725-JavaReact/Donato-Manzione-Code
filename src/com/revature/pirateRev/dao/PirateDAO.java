6410ec992bf991a9e4799a318943a8d408b40cc9
Donato <donato.manz.sc@gmail.com>
2022-08-01 16:44:58 -0400

    DAO interface, dao package, and PirateDAO class without implementations

---
+++ src/com/revature/pirateRev/dao/PirateDAO.java
@@ -0,0 +1,31 @@
+package com.revature.pirateRev.dao;
+
+import com.revature.pirateRev.models.Pirate;
+
+public class PirateDAO implements DAO<Pirate> {
+
+	@Override
+	public void create(Pirate obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Pirate readByName(String name) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void update(Pirate obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(String name) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
