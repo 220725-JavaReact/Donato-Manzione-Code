68cb80163ac07a0c6686c061dc3a0bc9e790bb92
Donato <donato.manz.sc@gmail.com>
2022-08-07 10:20:18 -0400

    created all dao classes, but they're still empty

---
+++ src/main/com/revature/pirateRev/dao/LineOrderDAO.java
@@ -0,0 +1,36 @@
+package main.com.revature.pirateRev.dao;
+import main.com.revature.pirateRev.models.StoreFront;
+
+public class LineOrderDAO implements DAO<StoreFront> {
+
+	@Override
+	public void create(StoreFront obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public StoreFront readByName(String name) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void update(StoreFront obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(String name) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Object[] readAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/main/com/revature/pirateRev/dao/OrderDAO.java
@@ -0,0 +1,36 @@
+package main.com.revature.pirateRev.dao;
+import main.com.revature.pirateRev.models.Order;
+
+public class OrderDAO implements DAO<Order> {
+
+	@Override
+	public void create(Order obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Order readByName(String name) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void update(Order obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(String name) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Object[] readAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
